ab6713e56eedc32200cb1b98be0390d1161ecfbc
WALLESAMEHERE <hulk13@op.pl>
2017-06-20 00:42:36 +0200

    several corections

--- 2Task/app.js
+++ 2Task/app.js
@@ -6,60 +6,60 @@ window.onload = function() {
         var lowerSpell = yourSpell.toLowerCase();
         const startSpell = 'fe';
         const endSpell = 'ai';
-        var result = 0;
+        var damage = 0;
+        var arrayWithSubspells = [
+            ['fe', 1],
+            ['ain', 2],
+            ['jee', 3],
+            ['dai', 5],
+            ['je', 2],
+            ['ne', 2],
+            ['ai', 2],
+        ];
         if (lowerSpell.search(startSpell) === -1 || lowerSpell.search(endSpell) === -1) {
-            document.getElementById('doneDamage').innerHTML = result;
+            document.getElementById('doneDamage').innerHTML = damage;
         } else {
-            var stringSpell = lowerSpell.match(startSpell + '.*');
-            var spelos = stringSpell[0];;
-            var lastSpell = spelos.lastIndexOf('ai')
-            var rightSpell = spelos.substring(0, lastSpell + 2);
-            checkSpell(rightSpell);
-            function checkSpell(spell){
+            var spellFromStart = lowerSpell.match(startSpell + '.*');
+            var lastSpell = spellFromStart[0].lastIndexOf(endSpell);
+            var stringSpell = spellFromStart[0].substring(0, lastSpell + 2);
+            exports.damage(stringSpell, damage, arrayWithSubspells);
+        }
+    });
+    var exports = {
+        damage: (spell, damage, arrayWithSubspells) => {
             if (spell.startsWith('fe', 0) && spell.endsWith('ai', spell.length)) {
-                var tabla = [
-                    ['fe', 1],
-                    ['ain', 2],
-                    ['jee', 3],
-                    ['dai', 5],
-                    ['je', 2],
-                    ['ne', 2],
-                    ['ai', 2],
-                ];
-                
-                var stringToZmiany = spell;
-                var count = 0;
-                var stringLength = stringToZmiany.length;
+                var stringForNoSpells = spell;
+                var stringLength = stringForNoSpells.length;
                 var doubleFe = 0;
-                for (var i = 0; i < tabla.length; i++) {
+                for (var i = 0; i < arrayWithSubspells.length; i++) {
                     let count = 0;
-                    var pos = stringToZmiany.indexOf(tabla[i][0]);
-                    var scorejest = 0;
+                    var pos = stringForNoSpells.indexOf(arrayWithSubspells[i][0]);
                     while (pos > -1) {
                         ++count;
-                        pos = stringToZmiany.indexOf(tabla[i][0], ++pos);
-                        stringLength -= tabla[i][0].length;
-                        console.log(count, tabla[i][0]);
-                        if (tabla[i][0] === 'fe') {
+                        pos = stringForNoSpells.indexOf(arrayWithSubspells[i][0], ++pos);
+                        stringLength -= arrayWithSubspells[i][0].length;
+                        console.log(count, arrayWithSubspells[i][0]);
+                        if (arrayWithSubspells[i][0] === 'fe') {
                             ++doubleFe;
                         }
-                        for (var j = 0; j < tabla.length; j++) {
-                            if (tabla[i][0].length > 2 && tabla[i][0].includes(tabla[j][0]) && stringToZmiany.includes(tabla[j][0])) {
-                                result -= 2;
+                        for (var j = 0; j < arrayWithSubspells.length; j++) {
+                            if (arrayWithSubspells[i][0].length > 2 && arrayWithSubspells[i][0].includes(arrayWithSubspells[j][0]) && stringForNoSpells.includes(arrayWithSubspells[j][0])) {
+                                damage -= 2;
                             }
                         }
                     }
-                    result += count * tabla[i][1];
+                    damage += count * arrayWithSubspells[i][1];
                 }
-                result -= stringLength;
+                damage -= stringLength;
                 if (doubleFe > 1) {
-                    result = 0;
+                    damage = 0;
                 }
-                document.getElementById('doneDamage').innerHTML = result;
+                document.getElementById('doneDamage').innerHTML = damage;
+
             } else {
-                document.getElementById('doneDamage').innerHTML = result;
+                document.getElementById('doneDamage').innerHTML = damage;
             }
-            }
+            return damage;
         }
-    });
+    }
 }
